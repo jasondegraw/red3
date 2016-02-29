a97e069070ff98333f61d6c7aa7a5c10ca9fa0bf
Jason W. DeGraw <jason.degraw@gmail.com>
2016-02-28 23:16:20 -0700

    Rename files

--- src/staggeredmodel.cpp
+++ src/staggeredmodel.cpp
@@ -43,7 +43,41 @@ StaggeredModel::StaggeredModel(unsigned nx, unsigned ny, unsigned nz)
   if(nz > 1) {
     nuvw += nz+1;
   }
-
+  //s->x = s->y = s->z = 0;
+  // s->dx = s->dy = s->dz = 0;
+  // s->dx = (real_t*)calloc(s->nx,sizeof(real_t));
+  // if(s->dx == NULL)return 0;
+  // s->dy = (real_t*)calloc(s->ny,sizeof(real_t));
+  // if(s->dy == NULL)return 0;
+  // if(nz == 1)  /* Two Dimensions */
+    // {
+      // s->dim = 2;
+      // s->nw  = 0;
+     // s->z   = 0;
+      // s->w   = 0;
+      //s->iv  = (nx+1)*(ny+2);
+      //s->iw  = 0;
+      //s->nsa = s->iv+(nx+2)*(ny+1);
+      // s->nuvw = s->nsa;
+      // s->u   = (real_t*)calloc(s->nsa,sizeof(real_t));
+      // if(s->u == NULL)return 0;
+      // s->v   = s->u + s->iv;
+    // }
+  // else  /* Three Dimensions */
+    // {
+      // /* Do it! */
+      // /*
+          // s->w = (real_t*)malloc(sizeof(real_t)*((nx+2)*(ny+1)*nz));
+          // if(s->w == NULL)return 0;
+      // */
+    // }
+  // /*
+    // Allocate the pressure array
+  // */
+  // s->ncell = nx*ny*nz;
+  // /* if(s->xperi)s->ncell = (nx+1)*ny*nz; */
+  // s->p = (real_t*)calloc(s->ncell,sizeof(real_t));
+  // if(s->p == NULL)return 0;
 }
 
 }
